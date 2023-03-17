eac69019644dcf42cb3585a578c23542edf34210
Yiding Wang <yiding.wang82@gmail.com>
2023-03-17 16:14:16 -0400

    DeliverableTwo

---
+++ src/TheCoinFlipChallenge.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Random;
+public class TheCoinFlipChallenge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Welcome to the COIN FLIP CHALLENGE!");
+     Scanner obj=new Scanner(System.in);
+     System.out.println("What is your name?");
+     String name=obj.nextLine();
+     System.out.println("Welcome "+name+". Do you want to do the COIN FLIP CHALLENGE? Yes/No");
+     String answer1=obj.nextLine().toLowerCase();
+     int score=0,j=0;
+	 if(answer1.equals("no")) {
+		 System.out.println("You are a coward "+name);
+	 }else if (answer1.equals("yes")){
+		 
+		for(int i=0;i<=5;i++) {
+			Random random=new Random();
+			int rand=random.nextInt(2);
+			
+			System.out.println("Heads or Tails?");
+			String answer2=obj.nextLine().toLowerCase();
+			String result=(rand==1&&answer2.equals("heads"))? "Correct!" : "Wrong!";
+			System.out.println(result);
+			score=(result.toLowerCase().equals("correct!"))? j++ : j;	
+		}
+		System.out.println("Thank you "+name+". You got a score of "+score+"!");
+		}
+		
+	}
+
+}
